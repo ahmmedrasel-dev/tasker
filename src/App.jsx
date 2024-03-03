07613c53314899273413b36300c24f06acb5e4b2
ahmmedrasel-dev <rahmmed.info@gmail.com>
2024-03-03 22:54:25 +0600

    conver html to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <body className="bg-[#191D26] font-[Inter] text-white">
+        <Header />
+        <Home />
+        <Footer />
+      </body>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import TaskContainer from "../components/TaskContainer";
+
+const Home = () => {
+  return (
+    <>
+      <HeroSection />
+      <TaskContainer />
+    </>
+  );
+};
+
+export default Home;
